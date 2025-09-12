df91da2ac352a24ecd51ac602d24f4445cb04600
Titikorn <titikorn@Mac.ST-244F>
2025-09-12 22:04:28 +0700

    fix bug authRotes.js

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,24 +1,36 @@
 // routes/auth.js
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { body } = require('express-validator');
-const authControllers = require('../controllers/authControllers');
+const { body } = require("express-validator");
+const authControllers = require("../controllers/authControllers");
+const { requireAuth } = require("../middleware/authMiddleware");
 
-router.post('/register', [
-  body('username').isLength({ min: 3 }).withMessage('username อย่างน้อย 3 ตัว'),
-  body('email').isEmail().withMessage('email ไม่ถูกต้อง'),
-  body('password').isLength({ min: 6 }).withMessage('password อย่างน้อย 6 ตัว'),
-  body('passwordConfirm').exists()
-], authControllers.register);
+router.post(
+  "/register",
+  [
+    body("username")
+      .isLength({ min: 3 })
+      .withMessage("username อย่างน้อย 3 ตัว"),
+    body("email").isEmail().withMessage("email ไม่ถูกต้อง"),
+    body("password")
+      .isLength({ min: 6 })
+      .withMessage("password อย่างน้อย 6 ตัว"),
+    body("passwordConfirm").exists(),
+  ],
+  authControllers.register
+);
 
-router.post('/login', [
-  body('email').isEmail(),
-  body('password').exists()
-], authControllers.login);
+router.post(
+  "/login",
+  [body("email").isEmail(), body("password").exists()],
+  authControllers.login
+);
 
-router.post('/verify-otp', [
-  body('otpCode').isLength({ min: 4 })
-], authControllers.verifyOtp);
+router.post(
+  "/verify-otp",
+  [body("otpCode").isLength({ min: 4 })],
+  authControllers.verifyOtp
+);
 
 // Protected route (ต้องมี JWT จริง)
 router.get("/me", requireAuth, (req, res) => {
